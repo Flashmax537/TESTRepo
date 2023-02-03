1f9930d798591f01b8dffe5c7c3d27acb2cc95ac
FlashMax <flashmax537@yandex.ru>
2023-02-03 13:07:24 +0400

    add class

---
+++ TESTProj/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTProj
+{
+    public class TestClass
+    {
+        public string GetTest(string testArg)
+        {
+            return testArg;
+        }
+    }
+}
